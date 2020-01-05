2d7daa3d7d3035ed0c540d0a28463d143af56316
unknown <niloufar>
2020-01-05 17:11:05 +0330

    first structure

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
-
+struct date 
+{
+	int day;
+	int month;
+	int year;
+};
+struct sans 
+{
+	char sansName[20];
+	struct date dateStart[10];
+	struct date dateEnd[10];
+	int startTime[10];
+	int endTime[10];
+	char movieName[50];
+	int emptySets[10];
+};
 int main()
 {
-	print("Hi Nil");
-	return 0;
+	struct sans s={Motreb};
+	printf ("%s", s.sansName);
 }
